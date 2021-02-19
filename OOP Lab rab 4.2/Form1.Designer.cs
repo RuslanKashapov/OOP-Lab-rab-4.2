3c638f3a68aa5233d31d2e5361bf74d96421d9a6
Руслан Кашапов <k.ruslan.ufa@gmail.com>
2021-02-19 10:37:14 +0500

    Окончательный

--- OOP Lab rab 4.2/Form1.Designer.cs
+++ OOP Lab rab 4.2/Form1.Designer.cs
@@ -28,54 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(567, 157);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(120, 23);
-            this.progressBar1.TabIndex = 0;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(567, 219);
+            this.numericUpDown1.Location = new System.Drawing.Point(210, 103);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
             this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(399, 103);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(567, 111);
+            this.textBox1.Location = new System.Drawing.Point(210, 147);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(120, 22);
-            this.textBox1.TabIndex = 2;
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(399, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.progressBar1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- OOP Lab rab 4.2/Form1.cs
+++ OOP Lab rab 4.2/Form1.cs
@@ -12,9 +12,77 @@ namespace OOP_Lab_rab_4._2
 {
     public partial class Form1 : Form
     {
+        Model model;
+
         public Form1()
         {
             InitializeComponent();
+            model = new Model();
+            model.observer += new System.EventHandler(this.UpdateFromModel);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            model.setValue1(Decimal.ToInt32(numericUpDown1.Value));
+            /*if (numericUpDown1.Value <= numericUpDown2.Value)
+                numericUpDown2.Value = numericUpDown1.Value - 1;
+            textBox1.Text = numericUpDown1.Value.ToString();
+            label1.Text = numericUpDown1.Value.ToString();*/
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            model.setValue2(Decimal.ToInt32(numericUpDown2.Value));
+            /*if (numericUpDown2.Value >= numericUpDown1.Value)
+                numericUpDown1.Value = numericUpDown1.Value + 1;
+            textBox2.Text = numericUpDown2.Value.ToString();
+            label2.Text = numericUpDown2.Value.ToString();*/
+        }
+
+        private void UpdateFromModel(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = model.getValue1();
+            textBox1.Text = model.getValue1().ToString();
+            label1.Text = model.getValue1().ToString();
+            numericUpDown2.Value = model.getValue2();
+            textBox2.Text = model.getValue2().ToString();
+            label2.Text = model.getValue2().ToString();
         }
     }
+
+    public class Model
+    {
+        private int value1;
+        private int value2;
+        public System.EventHandler observer;
+
+        public void setValue1(int value)
+        {
+            value1 = value;
+            if (value1 <= value2)
+            {
+                value2 = value1 - 1;
+            }
+            observer.Invoke(this, null);
+        }
+        public void setValue2(int value)
+        {
+            value2 = value;
+            if (value2 >= value1)
+            {
+                value1 = value2 + 1;
+            }
+            observer.Invoke(this, null);
+        }
+        public int getValue1()
+        {
+            return value1;
+        }
+
+        public int getValue2()
+        {
+            return value2;
+        }
+    }
+
 }
